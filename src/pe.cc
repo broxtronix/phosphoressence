10a8ae56faef97e3a16a80b013f74fa7e1ebada4
Michael Broxton <michael.broxton@nasa.gov>
2010-03-23 03:29:29 -0700

    Started a branch for working on a PE/OF merge.

--- src/pe.cc
+++ src/pe.cc
@@ -26,6 +26,43 @@
 /// An audio and video delight.
 ///
 
+// #include <iostream>
+// #include "cv.h"
+// #include "highgui.h"
+
+// using namespace cv;
+
+// int main(int argc, char** argv)
+// {
+//   VideoCapture cap;
+//   if (argc == 2) {
+//     cap.open(argv[1]); 
+//   } else {
+//     cap.open(0); 
+//   }
+
+//   // check if we succeeded  
+//   if(!cap.isOpened()) {
+//     std::cout << "Could not open video camera 0.  Exiting.\n";
+//     exit(1);
+//   }
+
+//   Mat edges;
+//   namedWindow("edges",1);
+//   for(;;) {
+//     Mat frame;
+//     cap >> frame; // get a new frame from camera
+//     cvtColor(frame, edges, CV_BGR2GRAY);
+//     GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+//     Canny(edges, edges, 0, 30, 3);
+//     imshow("edges", edges);
+//     if(waitKey(30) >= 0) break;
+//   }
+//   // the camera will be deinitialized automatically in VideoCapture destructor
+//   return 0;
+// }
+
+
 // Qt
 #include <QApplication>
 #include <QWidget>
