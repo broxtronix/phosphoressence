11abf5a159dbb96d88d047d6ad5ce39ec8c87c57
Michael Broxton <michael.broxton@nasa.gov>
2010-04-27 04:43:06 -0700

    Video capture is now displaying properly as a texture.  Contour finding overlaid on top.

--- src/VideoEngine.cc
+++ src/VideoEngine.cc
@@ -37,8 +37,9 @@ using namespace cv;
 class VideoTask {
   pe::Mutex m_mutex;
   bool m_terminate;
+  float m_aspect_ratio;
   boost::shared_ptr<cv::VideoCapture> m_video_capture;
-  cv::Mat m_current_frame, m_prev_frame;
+  cv::Mat m_raw_frame, m_prev_frame, m_background_frame;
   pe::graphics::Texture m_video_texture;
   pe::vision::ContourFinder m_contour_finder;
   pe::vision::BlobTracker m_blob_tracker;
@@ -46,42 +47,38 @@ class VideoTask {
 public:
   VideoTask(pe::Vector2 resolution) : m_terminate(false) {
 
-    // // Plays a file on disk.
-    // if (url.size() != 0) {
-    //   m_video_capture.reset(new cv::VideoCapture(url));
+    m_video_capture.reset(new cv::VideoCapture(0));
+    
+    // check if we succeeded  
+    if(!m_video_capture->isOpened()) {
+      std::cout << "Could not open video camera 0.  Exiting.\n";
+      exit(1);
+    }
 
-    //   // check if we succeeded  
-    //   if(!m_video_capture->isOpened()) {
-    //     std::cout << "Could not open video file: " << url << "\n";
-    //     exit(1);
-    // }
+    // Set capture size
+    m_video_capture->set(CV_CAP_PROP_FRAME_WIDTH, resolution[0]);
+    m_video_capture->set(CV_CAP_PROP_FRAME_WIDTH, resolution[1]);
 
-    // // Opens the webcam.
-    // } else {
-      m_video_capture.reset(new cv::VideoCapture(0));
-
-      // check if we succeeded  
-      if(!m_video_capture->isOpened()) {
-        std::cout << "Could not open video camera 0.  Exiting.\n";
-        exit(1);
-      }
-    // }
 
     // Seed the first frame
     cv::Mat frame;
     (*m_video_capture) >> frame;
-    cvtColor(frame, m_current_frame, CV_BGR2GRAY);
+    cvtColor(frame, m_raw_frame, CV_BGR2GRAY);
+
+    // Save the background image & set the intial previous frame.
+    m_prev_frame = m_raw_frame;
+    m_background_frame = m_raw_frame;
 
     // Allocate texture memory
     m_video_texture.allocate(frame.size().width, frame.size().height, GL_RGB);
 
     // Set up the camera calibration matrix
-    float aspect_ratio = resolution.x()/resolution.y();
+    m_aspect_ratio = resolution.x()/resolution.y();
     pe::Matrix3x3 camera_calibration;
     camera_calibration.set_identity();
-    camera_calibration(0,0) = -2.0*aspect_ratio/640.0;
-    camera_calibration(1,1) = -2.0/480.0;
-    camera_calibration(0,2) = aspect_ratio;
+    camera_calibration(0,0) = -2.0*m_aspect_ratio/resolution[0];
+    camera_calibration(1,1) = -2.0/resolution[1];
+    camera_calibration(0,2) = m_aspect_ratio;
     camera_calibration(1,2) = 1.0;
     m_contour_finder.setCalibration(camera_calibration);
   }
@@ -92,50 +89,51 @@ public:
       pe::Mutex::Lock lock(m_mutex);
 
       // Store the previous frame
-      m_prev_frame = m_current_frame;
+      m_prev_frame = m_raw_frame;
       
       // Get a new frame from camera
       (*m_video_capture) >> frame; 
-      cvtColor(frame, m_current_frame, CV_BGR2GRAY);
+      cvtColor(frame, m_raw_frame, CV_BGR2GRAY);
       
+      cv::Mat working_frame = m_raw_frame.clone();
       // Todo: add background subtraction.
 
       // Blur a bit
-      GaussianBlur(m_current_frame, m_current_frame, cv::Size(7,7), 1.5, 1.5);
+      GaussianBlur(working_frame, working_frame, cv::Size(7,7), 1.5, 1.5);
       
       // Compute the threhold image.
       float thresh = pe_script_engine().get_parameter("vision_threshold");
       //      float thresh = 0.5;
-      cv::threshold(m_current_frame, m_current_frame, 255*thresh, 255, cv::THRESH_BINARY);
+      cv::threshold(working_frame, working_frame, 255*thresh, 255, cv::THRESH_BINARY);
 
-      imshow("test", m_current_frame);
+      //      imshow("test", working_frame);
 
       // Find Contours
-      m_contour_finder.findContours(m_current_frame,
+      m_contour_finder.findContours(working_frame,
                                     100,     // Min area
                                     640*480, // Max area
-                                    //100,
-                                    pe_script_engine().get_parameter("vision_num_blobs"),      // nConsidered
+                                    10,
+                                    //pe_script_engine().get_parameter("vision_num_blobs"),      // nConsidered
                                     false,   // find holes
                                     true);  // Use approximation
 
       // Track blobs
       m_blob_tracker.trackBlobs(m_contour_finder.blobs());
 
-      // Record frame at 60 Hz, tops!
+      // Record frames at 60 Hz, tops!
       pe::Thread::sleep_ms(16);
     } 
   }
 
   void draw(int x, int y, int width, int height) {
     pe::Mutex::Lock lock(m_mutex);
-    m_video_texture.loadData(m_current_frame.ptr(), 
-                             m_current_frame.size().width, 
-                             m_current_frame.size().height, 
+    m_video_texture.loadData(m_raw_frame.ptr(), 
+                             m_raw_frame.size().width, 
+                             m_raw_frame.size().height, 
                              GL_LUMINANCE, GL_UNSIGNED_BYTE);
 
-    // m_video_texture.draw(-1.0, -1.0, 1.0, 1.0);
-    m_contour_finder.draw(x, y, width, height);
+    m_video_texture.draw(m_aspect_ratio, -1.0, -2*m_aspect_ratio, 2.0);
+    m_contour_finder.draw();
   }
 
   void terminate() { m_terminate = true; }
